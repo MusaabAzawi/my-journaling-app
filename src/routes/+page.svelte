66e5b159337b823edf6e8bb590762508da37d5f3
MusaabAzawi <musaab.azawi@student.nhlstenden.com>
2025-06-23 17:37:36 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p><a href="/journaling">Check out the tutorial</a> to learn how to build SvelteKit apps</p>
